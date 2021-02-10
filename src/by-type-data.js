d6eb9c11b64c56f6805a1f4cde402867b90b7327
Olga Stratii <olga.stratii@strikersoft.com>
2021-02-10 12:00:31 +0200

    bubble chart legend

---
+++ src/by-type-data.js
@@ -0,0 +1,27 @@
+export const data = {
+    "name": "Термінові доставки крові",
+    "color": "#86eae9",
+    "children": [
+
+        {
+            "name": "Дитяче онко",
+            "loc": 6,
+            color: "#fde0d2"
+        },
+        {
+            "name": "Доставка крові",
+            "loc": 5,
+            color: "#fcbba1"
+        },
+        {
+            "name": "Доросле онко",
+            "loc": 11,
+            color: "#fb9272"
+        },
+        {
+            "name": "На операцію",
+            "loc": 6,
+            color: "#fa6a4a"
+        },
+    ]
+};
--- src/components/BubbleChart.js
+++ src/components/BubbleChart.js
@@ -1,60 +1,37 @@
-import { ResponsiveBubble } from '@nivo/circle-packing'
+import {ResponsiveBubble} from '@nivo/circle-packing'
+import {data} from '../by-type-data';
 import './Charts.css';
 
 export const BubbleChart = () => {
-    const root = {
-        "name": "Термінові доставки крові",
-        "color": "#86eae9",
-        "children": [
-
-            {
-                "name": "Дитяче онко",
-                "loc": 6
-            },
-            {
-                "name": "Доставка крові",
-                "loc": 5
-            },
-            {
-                "name": "Доросле онко",
-                "loc": 11
-            },
-            {
-                "name": "На операцію",
-                "loc": 6
-            },
-        ]
-    };
     return (
-        <div className='bubble'>
-            <ResponsiveBubble
-                root={root}
-                margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-                identity="name"
-                value="loc"
-                colorBy="name"
-                colors={{ scheme: 'reds' }}
-                padding={6}
-                labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 0.9 ] ] }}
-                borderWidth={2}
-                borderColor={{ from: 'color' }}
-                //labelTextColor="#fff"
-                defs={[
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'none',
-                        color: 'inherit',
-                        rotation: -45,
-                        lineWidth: 8,
-                        spacing: 9
-                    }
-                ]}
-                //fill={[ { match: { depth: 1 }, id: 'lines' } ]}
-                animate={true}
-                motionStiffness={90}
-                motionDamping={12}
-            />
+        <div className='bubble-container'>
+            <div className='bubble'>
+                <ResponsiveBubble
+                    root={data}
+                    margin={{top: 20, right: 20, bottom: 20, left: 20}}
+                    identity="name"
+                    value="loc"
+                    colorBy="name"
+                    colors={{scheme: 'reds'}}
+                    padding={6}
+                    labelTextColor={{from: 'color', modifiers: [['darker', 0.9]]}}
+                    borderWidth={4}
+                    borderColor={{from: 'color', modifiers: [['darker', 0.2]]}}
+                    animate={true}
+                    motionStiffness={90}
+                    motionDamping={12}
+                />
+            </div>
+            <ul className="bubble-legend">
+                {data.children.length ?
+                    data.children.map(i => (
+                            <li>
+                                <span className='legend-circle' style={{backgroundColor: i.color}}/>
+                                {i.name} - <b>{i.loc}</b>
+                            </li>
+                        )
+                    ) : null}
+            </ul>
         </div>
     )
 }
